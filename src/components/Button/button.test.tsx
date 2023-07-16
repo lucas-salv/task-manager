2e496f65f1c86866b7703761c6111d00dca1e097
Lucas Salv <salv.dev.br@gmail.com>
2023-07-16 18:55:08 -0300

    test(Button): add should render Button component test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,7 +1,11 @@
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 
 import { Button } from './';
 
 describe('<Button />', () => {
-  it('', () => {});
+  it('should render Button component', () => {
+    render(<Button />);
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
 });
