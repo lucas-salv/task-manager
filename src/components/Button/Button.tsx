58fb32aca75acbe9dc750edde8cf1d8a72962b3d
Lucas Salv <salv.dev.br@gmail.com>
2023-07-16 22:19:11 -0300

    style(Button): add default styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,7 @@
 export const Button = () => {
-  return <button>Button</button>;
+  return (
+    <button className="h-10 bg-secondary-100 px-4 rounded-md text-primary-300 font-bold hover:bg-secondary-200 active:bg-secondary-300 transition-colors">
+      Button
+    </button>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,15 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
     },
     colors: {
       transparent: 'transparent',
@@ -20,23 +20,27 @@ module.exports = {
       primary: {
         100: '#382479',
         200: '#2E1B6B',
-        300: '#231355',
+        300: '#231355'
       },
-      secondary: '#2CC78F',
+      secondary: {
+        100: '#2CC78F',
+        200: '#25A778',
+        300: '#208E67'
+      },
       info: '#4299E1',
       success: '#2CC78F',
       warning: '#ECC94B',
       error: '#F56565',
       textColor: 'rgba(255, 255, 255, 0.36)',
-      borderColor: '#382479',
+      borderColor: '#382479'
     },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
