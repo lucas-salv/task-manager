495844d2122a5a87e0ff478975f9a696af2858d5
Lucas Salv <salv.dev.br@gmail.com>
2023-07-17 22:11:36 -0300

    docs(Button): add argTypes config for leftIcon prop

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,10 +1,17 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { FaReact } from 'react-icons/fa';
 
 import { Button } from './';
 
 const meta: Meta<typeof Button> = {
   title: 'components/Button',
-  component: Button
+  component: Button,
+  argTypes: {
+    leftIcon: {
+      control: { type: 'boolean' },
+      mapping: { false: '', true: FaReact }
+    }
+  }
 };
 
 export default meta;
