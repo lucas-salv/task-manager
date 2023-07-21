797fec76c5472d2ca395c026932fabe1fb58c086
Lucas Salv <salv.dev.br@gmail.com>
2023-07-20 21:26:15 -0300

    feat(Input): add initial InputLabel structure

---
+++ src/components/Input/InputLabel/InputLabel.test.tsx
@@ -0,0 +1,5 @@
+import { screen, render } from '@testing-library/react';
+
+describe('<InputLabel />', () => {
+  it('should render Input with label', () => {});
+});
---
+++ src/components/Input/InputLabel/InputLabel.tsx
@@ -0,0 +1,3 @@
+export const InputLabel = () => {
+  return <label htmlFor="email">E-mail</label>;
+};
