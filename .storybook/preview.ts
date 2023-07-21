b838cc4e52b858da27be99a0cfcd8b6f56138ffa
Lucas Salv <salv.dev.br@gmail.com>
2023-07-21 20:33:18 -0300

    docs(Input): add default background theme to storybook preview file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,22 @@
 import type { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
 import './../src/app/globals.css';
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark
+    },
+    backgrounds: {
+      default: 'purple',
+      values: [
+        {
+          name: 'purple',
+          value: '#231355',
+        }
+      ]
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
