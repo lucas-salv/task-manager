7c5ac6c262d33231420a02e1d18ad295e041b1bf
Lucas Salv <salv.dev.br@gmail.com>
2023-07-28 21:58:56 -0300

    style(Switch): add default styles

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,8 +3,8 @@ import * as S from '@radix-ui/react-switch';
 
 export const Switch = () => {
   return (
-    <S.Root>
-      <S.Thumb />
+    <S.Root className="w-11 h-7 bg-primary-100 rounded-full data-[state=checked]:bg-secondary-100 transition-colors">
+      <S.Thumb className="block w-5 h-5 bg-white rounded-full translate-x-1 data-[state=checked]:translate-x-5 transition-transform" />
     </S.Root>
   );
 };
