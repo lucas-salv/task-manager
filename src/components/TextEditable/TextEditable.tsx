ab813ef0b64659ae8649c201d4b5d0436933a1bd
Lucas Salv <salv.dev.br@gmail.com>
2023-08-03 01:35:57 -0300

    feat(TextEditable): add edit button and its styles

--- src/components/TextEditable/TextEditable.tsx
+++ src/components/TextEditable/TextEditable.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { RiEdit2Fill } from 'react-icons/ri';
 
 type TextEditableProps = {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
@@ -7,5 +8,12 @@ type TextEditableProps = {
 
 export const TextEditable = ({ as = 'p', children }: TextEditableProps) => {
   const As = as;
-  return <As>{children}</As>;
+  return (
+    <As className="group flex items-center gap-2">
+      {children}
+      <button className="hidden px-1 group-hover:block">
+        <RiEdit2Fill />
+      </button>
+    </As>
+  );
 };
