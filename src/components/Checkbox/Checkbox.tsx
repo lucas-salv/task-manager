ca5cc8f43c844280455012c9e06d5ef29704e126
Lucas Salv <salv.dev.br@gmail.com>
2023-08-18 02:35:00 -0300

    feat(Checkbox): import primitive checkbox components and add initial styles to Checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,3 +1,12 @@
+import * as C from '@radix-ui/react-checkbox';
+import { BsCheck } from 'react-icons/bs';
+
 export const Checkbox = () => {
-  return <div></div>;
+  return (
+    <C.Root className="flex items-center justify-center w-5 h-5 bg-primary-100 rounded focus:bg-primary-200">
+      <C.Indicator className="text-white">
+        <BsCheck />
+      </C.Indicator>
+    </C.Root>
+  );
 };
