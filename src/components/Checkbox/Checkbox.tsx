3093173044f6879be7d08bca5562f1e37da2b061
Lucas Salv <salv.dev.br@gmail.com>
2023-08-18 02:40:47 -0300

    feat(Checkbox): add type props

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,7 +1,17 @@
 import * as C from '@radix-ui/react-checkbox';
 import { BsCheck } from 'react-icons/bs';
 
-export const Checkbox = () => {
+type CheckboxProps = {
+  defaultChecked?: boolean;
+  checked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+  disabled?: boolean;
+  required?: boolean;
+  name?: string;
+  value?: string;
+};
+
+export const Checkbox = ({}: CheckboxProps) => {
   return (
     <C.Root className="flex items-center justify-center w-5 h-5 bg-primary-100 rounded focus:bg-primary-200">
       <C.Indicator className="text-white">
