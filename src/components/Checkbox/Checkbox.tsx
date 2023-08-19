0089e18253475793c6e04a24dc6f9e69ed82e4d8
Lucas Salv <salv.dev.br@gmail.com>
2023-08-19 02:32:20 -0300

    style(Checkbox): add disabled styles

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -11,11 +11,15 @@ type CheckboxProps = {
   value?: string;
 };
 
-export const Checkbox = ({ ...props }: CheckboxProps) => {
+export const Checkbox = ({ disabled, ...props }: CheckboxProps) => {
+  const disabledStyles =
+    disabled && 'disabled:opacity-50 disabled:cursor-not-allowed';
+
   return (
     <C.Root
       {...props}
-      className="flex items-center justify-center w-5 h-5 bg-primary-100 rounded focus:bg-primary-200"
+      disabled={disabled}
+      className={`flex items-center justify-center w-5 h-5 bg-primary-100 rounded focus:bg-primary-200 ${disabledStyles}`}
     >
       <C.Indicator className="text-white">
         <BsCheck />
