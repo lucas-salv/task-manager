aa08181004db5b9062c908c6c581202179899ddf
Lucas Salv <salv.dev.br@gmail.com>
2023-08-21 20:48:00 -0300

    feat(Modal): import radix dialog components

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,20 @@
+import * as M from '@radix-ui/react-dialog';
+
+import { Button } from '../Button';
+
 export const Modal = () => {
-  return <div></div>;
+  return (
+    <M.Root>
+      <M.Trigger>
+        <Button>Open Modal</Button>
+      </M.Trigger>
+      <M.Portal>
+        <M.Overlay />
+        <M.Content>
+          <M.Title>Title</M.Title>
+          <M.Description>Lorem Ipsum dolor et</M.Description>
+        </M.Content>
+      </M.Portal>
+    </M.Root>
+  );
 };
