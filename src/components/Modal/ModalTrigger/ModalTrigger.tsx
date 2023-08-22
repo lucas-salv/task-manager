692ac9b49189ef23adc5915e8a186b6221b5b261
Lucas Salv <salv.dev.br@gmail.com>
2023-08-22 20:51:39 -0300

    feat(Modal): add children prop in Trigger

--- src/components/Modal/ModalTrigger/ModalTrigger.tsx
+++ src/components/Modal/ModalTrigger/ModalTrigger.tsx
@@ -1,3 +1,10 @@
-export const ModalTrigger = () => {
-  return <div></div>;
+import { Trigger } from '@radix-ui/react-dialog';
+import { ReactNode } from 'react';
+
+type TriggerProps = {
+  children: ReactNode;
+};
+
+export const ModalTrigger = ({ children }: TriggerProps) => {
+  return <Trigger>{children}</Trigger>;
 };
