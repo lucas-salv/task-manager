b7329212ecb4ecfd90e8dc48aa442f88f2491056
Lucas Salv <salv.dev.br@gmail.com>
2023-08-21 21:42:06 -0300

    style(Modal): add default styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,10 +9,12 @@ export const Modal = () => {
         <Button>Open Modal</Button>
       </M.Trigger>
       <M.Portal>
-        <M.Overlay />
-        <M.Content>
-          <M.Title>Title</M.Title>
-          <M.Description>Lorem Ipsum dolor et</M.Description>
+        <M.Overlay className="bg-black fixed inset-0 bg-opacity-50 backdrop-blur-sm  transition-opacity" />
+        <M.Content className="w-3/4 max-w-md max-h-screen p-7 bg-primary-200 rounded-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 focus:outline-none">
+          <M.Title className="text-white text-lg font-bold m-0">Title</M.Title>
+          <M.Description className="text-white mt-3 mb-5">
+            Lorem Ipsum dolor et
+          </M.Description>
         </M.Content>
       </M.Portal>
     </M.Root>
