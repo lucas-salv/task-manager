7799a79846555a9bab6abcd54fbf9804fe1160d7
Lucas Salv <salv.dev.br@gmail.com>
2023-08-26 01:55:59 -0300

    style(Modal): add hover and active ModalClose styles

--- src/components/Modal/ModalClose/ModalClose.tsx
+++ src/components/Modal/ModalClose/ModalClose.tsx
@@ -7,7 +7,7 @@ type ModalCloseProps = {
 
 export const ModalClose = ({ children }: ModalCloseProps) => {
   return (
-    <Close className="rounded-full w-6 h-6 inline-flex justify-center items-center text-white absolute top-2 right-2 bg-primary-100">
+    <Close className="rounded-full w-7 h-7 inline-flex justify-center items-center text-white absolute top-2 right-2 bg-primary-100 hover:bg-secondary-100 hover:text-primary-200 active:bg-secondary-300 transition-colors">
       {children}
     </Close>
   );
