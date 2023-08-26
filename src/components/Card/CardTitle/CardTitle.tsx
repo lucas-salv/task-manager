3ea629944cb96ef47ca0eadb8aac927883678af1
Lucas Salv <salv.dev.br@gmail.com>
2023-08-26 03:01:36 -0300

    feat(Card): add CardTitle props

--- src/components/Card/CardTitle/CardTitle.tsx
+++ src/components/Card/CardTitle/CardTitle.tsx
@@ -1,3 +1,15 @@
-export const CardTitle = () => {
-  return <h2></h2>;
+import { HTMLAttributes, ReactNode } from 'react';
+
+type CardTitleProps = {
+  children?: ReactNode;
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+} & HTMLAttributes<HTMLHeadingElement>;
+
+export const CardTitle = ({
+  children,
+  as = 'h2',
+  ...props
+}: CardTitleProps) => {
+  const As = as;
+  return <As {...props}>{children}</As>;
 };
