3bb22953d3d4b464a1480598d4c2cf3d6c510daf
Lucas Salv <salv.dev.br@gmail.com>
2023-08-26 02:44:40 -0300

    feat(Card): add Card props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,9 @@
-export const Card = () => {
-  return <div></div>;
+import { ReactNode } from 'react';
+
+type CardProps = {
+  children?: ReactNode;
+} & HTMLDivElement;
+
+export const Card = ({ children }: CardProps) => {
+  return <div>{children}</div>;
 };
