a2391d693c4a62a6e5e097a8985800e96e0d603c
Lucas Salv <salv.dev.br@gmail.com>
2023-08-26 01:30:49 -0300

    feat(Modal): add children prop to ModalClose

--- src/components/Modal/ModalClose/ModalClose.tsx
+++ src/components/Modal/ModalClose/ModalClose.tsx
@@ -1,3 +1,10 @@
-export const ModalClose = () => {
-  return <div></div>;
+import { Close } from '@radix-ui/react-dialog';
+import { ReactNode } from 'react';
+
+type ModalCloseProps = {
+  children?: ReactNode;
+};
+
+export const ModalClose = ({ children }: ModalCloseProps) => {
+  return <Close>{children}</Close>;
 };
