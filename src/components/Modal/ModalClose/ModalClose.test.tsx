92cba829dee8c0faf1169d70c9f73ebfa2e807b9
Lucas Salv <salv.dev.br@gmail.com>
2023-08-26 02:04:36 -0300

    test(Modal): add 'should render ModalClose inside ModalContent when opened' test

--- src/components/Modal/ModalClose/ModalClose.test.tsx
+++ src/components/Modal/ModalClose/ModalClose.test.tsx
@@ -1,5 +1,19 @@
 import { screen, render } from '@testing-library/react';
 
+import { Modal, ModalContent, ModalClose } from './../';
+
 describe('<ModalClose />', () => {
-  it('', () => {});
+  it('should render ModalClose inside ModalContent when opened', () => {
+    render(
+      <Modal defaultOpen={true}>
+        <ModalContent>
+          <ModalClose>Close Button</ModalClose>
+        </ModalContent>
+      </Modal>
+    );
+
+    expect(
+      screen.getByRole('button', { name: 'Close Button' })
+    ).toBeInTheDocument();
+  });
 });
