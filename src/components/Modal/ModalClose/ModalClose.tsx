c0b2a5c109584471c5d3b436a2717ae9e2eb5a06
Lucas Salv <salv.dev.br@gmail.com>
2023-08-26 01:38:18 -0300

    style(Modal): add ModalClose styles

--- src/components/Modal/ModalClose/ModalClose.tsx
+++ src/components/Modal/ModalClose/ModalClose.tsx
@@ -6,5 +6,9 @@ type ModalCloseProps = {
 };
 
 export const ModalClose = ({ children }: ModalCloseProps) => {
-  return <Close>{children}</Close>;
+  return (
+    <Close className="rounded-full w-6 h-6 inline-flex justify-center items-center text-white absolute top-2 right-2 bg-primary-100">
+      {children}
+    </Close>
+  );
 };
