25b6bc4d12053800b9d3d5ee302a46b532d640a1
Lucas Salv <salv.dev.br@gmail.com>
2023-08-27 01:24:21 -0300

    test(Card): add 'should render CardTitle inside Card' and 'should render CardContent inside Card' tests

--- src/components/Card/CardContent/CardContent.test.tsx
+++ src/components/Card/CardContent/CardContent.test.tsx
@@ -3,5 +3,12 @@ import { screen, render } from '@testing-library/react';
 import { Card, CardContent } from './../';
 
 describe('<ModalContent />', () => {
-  it('', () => {});
+  it('should render CardContent inside Card', () => {
+    render(
+      <Card>
+        <CardContent>Lorem ipsum</CardContent>
+      </Card>
+    );
+    expect(screen.getByText('Lorem ipsum')).toBeInTheDocument();
+  });
 });
--- src/components/Card/CardTitle/CardTitle.test.tsx
+++ src/components/Card/CardTitle/CardTitle.test.tsx
@@ -3,5 +3,13 @@ import { screen, render } from '@testing-library/react';
 import { Card, CardTitle } from './../';
 
 describe('<CardTitle />', () => {
-  it('', () => {});
+  it('should render CardTitle inside Card', () => {
+    render(
+      <Card>
+        <CardTitle>Title</CardTitle>
+      </Card>
+    );
+
+    expect(screen.getByRole('heading', { name: 'Title' })).toBeInTheDocument();
+  });
 });
